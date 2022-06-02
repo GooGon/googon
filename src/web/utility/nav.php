134a7f6354d07e767edb9fd97a0d98fc3e25d6b4
GooGon <haf.haikal@gmail.com>
2022-06-02 22:31:34 +0800

    new index nav

--- src/web/utility/nav.php
+++ src/web/utility/nav.php
@@ -1,18 +1,27 @@
-<nav class="navbar">  
-    <!-- LOGO -->
-    <div class="logo">GooGon</div>
-    <!-- NAVIGATION MENU -->
-    <ul class="nav-links" style="margin-bottom: 0rem;">
-        <input type="checkbox" id="checkbox_toggle" />
-        <label for="checkbox_toggle" class="hamburger">&#9776;</label>
-
-        <!-- NAVIGATION MENUS -->
-        <div class="menu">
-            <li><a href='<?php echo constant("URL"); ?>src/blog.php'>Blog</a></li>
-            <li><a href='<?php echo constant("URL"); ?>src/portofolio.php'>Portofolio</a> </li>
-            <li><a href='<?php echo constant("URL"); ?>src/curiculum_vitae.php'>Curiculum Vitae</a> </li>
-            <li><a href='<?php echo constant("URL"); ?>src/contact_me.php'>Contact Me</a></li>
-            <button><a href='<?php echo constant("URL"); ?>src/sign_in.php'>Login</a></button>
-        </div>
-    </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">GooGon</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href='<?php echo constant("URL"); ?>src/blog.php'>Blog</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href='<?php echo constant("URL"); ?>src/portofolio.php'>Portofolio</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href='<?php echo constant("URL"); ?>src/curiculum_vitae.php'>Curiculum Vitae</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href='<?php echo constant("URL"); ?>src/contact_me.php'>Contact Me</a>
+        </li>
+      </ul>
+      <form class="d-flex" action='<?php echo constant("URL"); ?>src/sign_in.php'>
+        <button class="btn btn-outline-success" type="submit">Sign In</button>
+      </form>
+    </div>
+  </div>
 </nav>
