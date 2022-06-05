2d215b492607d0b5b4a10dbb92b6dd874bef9967
GooGon <haf.haikal@gmail.com>
2022-06-05 20:27:44 +0800

    06-05-2022

--- src/web/utility/nav.php
+++ src/web/utility/nav.php
@@ -7,19 +7,19 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-            <a class="nav-link" aria-current="page" href='<?php echo constant("URL"); ?>src/web/blog.php'>Blog</a>
+            <a class="nav-link" aria-current="page" href='<?php echo URL; ?>src/web/blog.php'>Blog</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" aria-current="page" href='<?php echo constant("URL"); ?>src/web/portofolio.php'>Portofolio</a>
+            <a class="nav-link" aria-current="page" href='<?php echo URL; ?>src/web/portofolio.php'>Portofolio</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" aria-current="page" href='<?php echo constant("URL"); ?>src/web/curiculum_vitae.php'>Curiculum Vitae</a>
+            <a class="nav-link" aria-current="page" href='<?php echo URL; ?>src/web/curiculum_vitae.php'>Curiculum Vitae</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" aria-current="page" href='<?php echo constant("URL"); ?>src/web/contact_me.php'>Contact Me</a>
+            <a class="nav-link" aria-current="page" href='<?php echo URL; ?>src/web/contact_me.php'>Contact Me</a>
         </li>
       </ul>
-      <form class="d-flex" action='<?php echo constant("URL"); ?>src/sign_in.php'>
+      <form class="d-flex" action='<?php echo URL; ?>src/sign_in.php'>
         <button class="btn btn-outline-success" type="submit">Sign In</button>
       </form>
     </div>
