29bf274e31e0d237bfc08b504096c02bdb700c4e
GooGon <haf.haikal@gmail.com>
2022-06-13 14:14:12 +0800

    13/06/2022

--- config/define.php
+++ config/define.php
@@ -1,4 +1,8 @@
 <?php
 define("URL","http://127.0.0.1/googon/");
-define("WEB_NAME","GooGon Testing Website")
+define("WEB_NAME","GooGon Testing Website");
+define("LOCAL_HOST","");
+define("DB_NAME","");
+define("DB_USERNAME","");
+define("DB_PASSWORD","");
 ?>
